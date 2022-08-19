c69454746ab55d935e2eacc5370a59ccd8cf4584
SteLow313 <103936555+SteLow313@users.noreply.github.com>
2022-08-19 18:52:42 +0200

    Update Program.cs

--- Snow/Program.cs
+++ Snow/Program.cs
@@ -1,41 +1,19 @@
-﻿using System;
-using System.Runtime.InteropServices;
+using System;
 using System.Windows.Forms;
-using System.Threading;
-using System.Diagnostics;
 
 namespace Snow
 {
     internal static class Program
     {
         [STAThread]
-        [DllImport("kernel32.dll")]
-        static extern IntPtr GetConsoleWindow();
 
-        [DllImport("user32.dll")]
-        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
-
-        public const int SW_HIDE = 0;
-        public const int SW_SHOW = 5;
-        public static void HideConsole()
-        {
-            randomsHelper.hidden = true;
-            var handle = GetConsoleWindow();
-            ShowWindow(handle, SW_HIDE);
-        }
-        public static void ShowConsole()
-        {
-            randomsHelper.hidden = false;
-            var handle = GetConsoleWindow();
-            ShowWindow(handle, SW_SHOW);
-        }
         public static void Main()
         {
+            randomsHelper.HideConsole();
+
             voidsHelper.checkVersion();
             voidsHelper.getHwidStatus();
             voidsHelper.generateId();
-
-            HideConsole();
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
